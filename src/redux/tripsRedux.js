bbe1ddc2675176633c2fae04a7ab060d0003563f
Iza Witkowska <isa.witkowska@gmail.com>
2020-06-24 19:56:29 +0200

    Modified gerTRipsbyId

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -21,9 +21,8 @@ export const getFilteredTrips = ({trips, filters}) => {
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.id == tripId);
 
-  // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
