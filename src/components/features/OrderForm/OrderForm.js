9de75ec82ddf72f041f5bd85f5127fdeba59c212
Iza Witkowska <isa.witkowska@gmail.com>
2020-07-18 13:26:31 +0200

    Add OrderOptions -all Components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import propTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 
 class OrderForm extends React.Component {
@@ -15,6 +17,11 @@ class OrderForm extends React.Component {
     const {tripCost,options} = this.props;
     return (
       <Row>
+        {pricing.map(option =>
+          <Col md={4} key={option.id}>
+            <OrderOption {...option}>  </OrderOption>
+          </Col>
+        )}
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options}>         
           </OrderSummary>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './OrderOption';
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionNumber from './OrderOptionNumber';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
+
+
+ 
+export default OrderOption;
