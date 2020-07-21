fb4436df46cec988efa6e6fce169fdf90effe374
Iza Witkowska <isa.witkowska@gmail.com>
2020-07-21 19:08:43 +0200

    Tests fpr TripSummary

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should generate link to correct adress',() => {
+    const id = 'abc';
+    const component = shallow(<TripSummary id= {id}/>);
+    const correctAdress = component.find('.link').prop('to'); 
+    expect(correctAdress).toEqual(`/trip/${id}`);
+    
+  });
+
+  it('should check if image has correct src and alt', () => {
+    const expectedName = 'Lorem ipsum';
+    const expectedSrc = 'image.jpg';
+    const component = shallow(<TripSummary src={expectedSrc} alt={expectedName}/>);
+    expect(component.find('.image').prop('src')).toEqual(expectedSrc);  
+    expect(component.find('.image').prop('alt')).toEqual(expectedName); 
+  });
+
+  it('should check if name, cost and days render correct', () => {
+    const expectedName = 'Lorem ipsum';
+    const expectedDays = 2;
+    const expectedCost = '30';
+    const component = shallow(<TripSummary name={expectedName} days={expectedDays} cost={expectedCost}/>);
+
+    expect(component.find('.name')).toEqual(expectedName);
+    expect(component.find('.days')).toEqual(expectedDays);
+    expect(component.find('.cost')).toEqual(expectedCost);
+
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+
+  it('should test tag', () => {
+    const tagOne = '1';
+    const tagTwo = '2';
+    const tagThree = '3';
+    const tag = [tagOne, tagTwo, tagThree];
+    const component = shallow(<TripSummary tags={tag}/>);
+    expect(component.find('.tags span').at(0).get(0));
+    expect(component.find('.tags span').at(1).get(1));
+    expect(component.find('.tags span').at(2).get(2));
+  });
+
+  it('should not render if tags is fals or empty', () => {//totalnie nie wiem
+    const tag = ['tagOne', 'tagTwo', 'tagThree'];
+    const component = shallow(<TripSummary tags={tag}/>);
+    expect(component).toEqual;
+  });
+
+});
