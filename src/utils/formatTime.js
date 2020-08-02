59d334e7dee8ea97fcdc4fcc80ac2494b5b77909
Iza Witkowska <isa.witkowska@gmail.com>
2020-08-02 17:24:23 +0200

    formatTime almost ready

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,13 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = (time) => {
+  if(isNaN(time) || time < 0 || time == null) {
+    return null;
+  } else {
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor((time / 60) % 60);
+    const hours = Math.floor(time/ 3600);
+
+    return (hours) + ':' + (minutes) + ':' + (seconds);
+  }
+
+
+};
